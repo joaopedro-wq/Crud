9ab2853071ce67d6fc75388a2fbe945001439cfd
WillCQ97 <willian.cqueiroz@gmail.com>
2022-02-07 20:33:14 -0300

    refactoring folders

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const app = express();
+const port = 3001;
 
-app.listen(3001,() => (
+app.get("/", (req, res) => {
+    res = '<h1>Isto é um teste !!!</h1>'
+    console.log("executei")
+});
 
-    console.log("rodanso servidor")
+app.listen(port, () => (
+    console.log("Estou ouvindo na porta " + port + '.')
 ));
