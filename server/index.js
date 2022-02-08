556a2399edca2eb6e8126a7602e8d051c61eb25c
João Pedro  Bandeira <joao.bandeiraoliveira@gmail.com>
2022-02-07 21:13:54 -0300

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,7 +1,25 @@
 const express = require("express")
 const app = express();
+const mysql = require("mysql");
 
-app.listen(3001,() => (
+const db = mysql.createPool({
+    host:"localhost",
+    user:"root",
+    password:"root",
+    database:"crud",
+    port:"3306",
 
-    console.log("rodanso servidor")
+});
+
+app.get("/", (req, res) =>{
+    console.log("irei executar essa buçanha")
+    let SQL =
+        "INSERT INTO tabela(nome, cost, category) VALUES ('Feijao', '20.0', 'ALIMENTO')";
+        db.query(SQL, (err, result) =>{
+            console.log(err);
+        });
+});
+
+app.listen(3001, () => (
+    console.log("rodando servidor")
 ));
