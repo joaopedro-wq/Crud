0fd47732cf18eaa503f227a4117c35084a78d42a
João Pedro  Bandeira <joao.bandeiraoliveira@gmail.com>
2022-02-16 08:22:46 -0300

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -15,10 +15,11 @@ const db = mysql.createPool({
 app.use(cors());
 app.use(express.json());
 
+
 app.post("/register", (req, res ) => {
-    const {nome} = req.body;
-    const {cost} = req.body;
-    const {category} = req.body;
+  
+    const {nome, cost, category} = req.body;
+    
     //console.log(nome);
 
         let SQL = "INSERT INTO tabela(nome, cost, category) VALUES (?, ?, ?)";
@@ -28,9 +29,53 @@ app.post("/register", (req, res ) => {
 
         });
     });
+ 
+    
+  
+
+    app.put("/editItem", (req, res) => {
+      
+        const { idTabela } = req.body;
+        const { nome } = req.body;
+        const { cost } = req.body;
+        const { category } = req.body;
+        let SQL = "UPDATE tabela SET nome = ?, cost = ?, category = ? WHERE idTabela = ?";
+        db.query(SQL, [nome, cost, category, idTabela], (err, result) => {
+          if (err) {
+            res.send(err);
+          } else {
+            res.send(result);
+          }
+        });
+      });
+    
+
+app.get("/getItems/:idTabela", (req, res) => {
+    const idTabela = req.params.idTabela;
+    console.log(idTabela);
+    let SQL = "SELECT * from tabela WHERE idTabela = "+idTabela;
+    
+    db.query(SQL, (err, result) => {
+        if(err) console.log(err);
+        else res.send(result);
+
+    });
+});
+
+app.get("/getItems/", (req, res) => {
+  
+  let SQL = "SELECT * from tabela  ";
+  
+  db.query(SQL, (err, result) => {
+      if(err) console.log(err);
+      else res.send(result);
+
+  });
+});
+
 
 
 
 app.listen(3001, () => (
-    console.log("rodando servidor")
+    console.log("rodadndo servidor")
 ));
